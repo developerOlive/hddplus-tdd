edbf6db3631059ed50e22ee4a0d66e4195cab8cc
developerOlive <chloehiho@gmail.com>
2025-03-26 17:58:28 +0900

    feat: 포인트 예외 처리 클래스 추가 및 글로벌 핸들링 추가

--- src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
@@ -1,14 +1,43 @@
 package io.hhplus.tdd;
 
+import io.hhplus.tdd.exception.UserNotFoundException;
+import io.hhplus.tdd.exception.InsufficientBalanceException;
+import io.hhplus.tdd.exception.InvalidAmountException;
+import io.hhplus.tdd.exception.MaxPointExceededException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-class ApiControllerAdvice extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(value = Exception.class)
+class ApiControllerAdvice {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity.status(404).body(new ErrorResponse("404", e.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(400).body(new ErrorResponse("400", e.getMessage()));
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<ErrorResponse> handleInsufficientBalanceException(InsufficientBalanceException e) {
+        return ResponseEntity.status(400).body(new ErrorResponse("400", e.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidAmountException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidAmountException(InvalidAmountException e) {
+        return ResponseEntity.status(400).body(new ErrorResponse("400", e.getMessage()));
+    }
+
+    @ExceptionHandler(MaxPointExceededException.class)
+    public ResponseEntity<ErrorResponse> handleMaxPointExceededException(MaxPointExceededException e) {
+        return ResponseEntity.status(400).body(new ErrorResponse("400", e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
+        return ResponseEntity.status(500).body(new ErrorResponse("500", "An unexpected error occurred."));
     }
 }
---
+++ src/main/java/io/hhplus/tdd/exception/InvalidAmountException.java
@@ -0,0 +1,7 @@
+package io.hhplus.tdd.exception;
+
+public class InvalidAmountException extends PointException {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+}
